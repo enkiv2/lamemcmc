1eaa2c689d3c7e9d9f90629a52507e9ff68cc372
enkiv2 <john.ohno@gmail.com>
2012-11-28 18:59:24 -0500

    Whoops. Forgot to handle passthrough of other options to fe.lua.

--- ircmode.sh
+++ ircmode.sh
@@ -80,7 +80,7 @@ echo >> .$$
 # Subshell might have a different pid. (Haven't checked)
 pid=$$
 tail -s 0.5 -f .$$ | tee /dev/stderr | \
-	 (./fe.lua -nt -i && rm -f .$pid && kill $pid) | \
+	 (./fe.lua -nt -i $opts && rm -f .$pid && kill $pid) | \
 	 irc -c $channel $nick $server  >> .$$
 rm -f .$$
 

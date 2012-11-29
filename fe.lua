af071fd481c60e96f1fae30a3d79e9b245df3c61
enkiv2 <john.ohno@gmail.com>
2012-11-28 19:55:00 -0500

    Added invoker nick to command responses.

--- fe.lua
+++ fe.lua
@@ -130,7 +130,7 @@ function interactiveMode()
 				line=string.gsub(line, "^(<[^>]+> )", 
 					function (c) 
 						replynick=string.sub(c, 2, 
-							string.len(c)-2)
+							string.len(c)-2)..": "
 						return ""
 					end)
 			end
@@ -139,46 +139,48 @@ function interactiveMode()
 			-- Convert bot commands to irc commands
 			if string.find(line, "!")==1 then
 				if string.find(line, "!save")==1 then
+					print(replynick.."Saving...")
 					saveDB()
+					print(replynick.."Done!")
 				elseif string.find(line, "!replyrate")==1 then
 					temp=string.sub(line, string.len("!replyrate"))
 					if tonumber(temp)~=nil then
 						replyrate=tonumber(temp)
-						print("Reply rate set to "..temp.."%")
+						print(replynick.."Reply rate set to "..temp.."%")
 					else
-						print("Reply rate is "..replyrate.."%")
+						print(replynick.."Reply rate is "..replyrate.."%")
 					end
 				elseif string.find(line, "!autosaverate")==1 
 				then
 					temp=string.sub(line, string.len("!autosaverate"))
 					if tonumber(temp)~=nil then
 						saverate=tonumber(temp)
-						print("Autosave rate set to "..temp.."%")
+						print(replynick.."Autosave rate set to "..temp.."%")
 					else
-						print("Autosave rate is "..saverate.."%")
+						print(replynick.."Autosave rate is "..saverate.."%")
 					end
 				elseif string.find(line, "!replyratio") then
 					temp=string.sub(line, string.len("!replyratio"))
 					if tonumber(temp)~=nil then
 						replyratio=tonumber(temp)
-						print("Only responses with ranks under "+temp+" will now be printed.")
+						print(replynick.."Only responses with ranks under "+temp+" will now be printed.")
 					else
-						print("Only responses with ranks under "+replyratio+" will be printed.")
+						print(replynick.."Only responses with ranks under "+replyratio+" will be printed.")
 					end
 				elseif string.find(line, "!help") then
-					print("Available commands are: !help !replyrate !replyratio !nick !quit !say !save !autosaverate !join !part")
+					print(replynick.."Available commands are: !help !replyrate !replyratio !nick !quit !say !save !autosaverate !join !part")
 					if (string.len(line)>string.len("!help")) then
 						temp=string.sub(line, 7)
 						if temp=="replyrate" then
-							print("replyrate: show/set probability of replies, in percent")
+							print(replynick.."replyrate: show/set probability of replies, in percent")
 						elseif temp=="replyratio" then
-							print("replyratio: show/set the maximum ranking for a response to be printed")
+							print(replynick.."replyratio: show/set the maximum ranking for a response to be printed")
 						elseif temp=="autosaverate" then
-							print("autosaverate: show/set probability of auto-saving per line, in percent. Set to 0 to turn off autosave.")
+							print(replynick.."autosaverate: show/set probability of auto-saving per line, in percent. Set to 0 to turn off autosave.")
 						elseif temp=="save" then
-							print("save: save database, if saving is on")
+							print(replynick.."save: save database, if saving is on")
 						elseif temp~= "" then
-							print("I cannot help you with "..temp..". Figure it out on your own.")
+							print(replynick.."I cannot help you with "..temp..". Figure it out on your own.")
 						end
 					end
 				else
